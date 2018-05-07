4524d412bc03f1487fce423c95e62ba292a4df5c
huangbin <575395982@qq.com>
2018-05-07 22:12:38 +0800

    MOD 修改项目包命名结构

--- src/androidTest/java/com/jichuangtech/commonutils/ApplicationTest.java
+++ src/androidTest/java/com/jichuangtech/commonutils/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.starnet.commonutils;
+package com.jichuangtech.commonutils;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- src/test/java/com/jichuangtech/commonutils/ExampleUnitTest.java
+++ src/test/java/com/jichuangtech/commonutils/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.starnet.commonutils;
+package com.jichuangtech.commonutils;
 
 import org.junit.Test;
 
